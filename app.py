db4370d4df08ae434a32a992f969282da93e1892
whatcoloris <brian.mcdorkle@gmail.com>
2022-11-12 12:04:21 -0500

    confusing, is pages working

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ freezer = Freezer(app)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "Hello World!!"
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
